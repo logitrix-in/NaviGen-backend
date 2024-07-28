d0e6bc49b07aa36d7effd07069b472144f38bd6c
ripan-roy <ripanroy111@gmail.com>
2024-07-28 16:02:01 +0530

    fixed

--- authentication/views.py
+++ authentication/views.py
@@ -1,8 +1,8 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
-
-from rest_framework.permissions import IsAuthenticated
+from django.contrib.auth import authenticate
+from rest_framework import status
 from django.contrib.auth.hashers import make_password
 from authentication.models import User
 from authentication.serializers import UserSerialisers
